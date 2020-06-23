ecd8a8e86889db45954c795b24c4338610a7ee44
Anshul Saxena <anshulc593@gmail.com>
2020-06-23 13:30:00 +0000

    First Commit

---
+++ test-mta-js/server.js
@@ -0,0 +1,73 @@
+var express = require("express");
+var app = express();
+
+var xsenv = require("@sap/xsenv");
+var hdbext = require("@sap/hdbext");
+
+var hanaOptions = xsenv.getServices({
+	hana: {
+		tag: "hana"
+	}
+});
+app.use(
+	hdbext.middleware(hanaOptions.hana)
+);
+
+app.get('/', function (req, res) {
+	res.type("text/html").status(200).send(
+		'<html><head></head><body><a href="/database">Database</a><br/><a href="/session">Session</a><br/><a href="/insert">Insert</a><br/><a href="/select">Select</a><br/></body></html>'
+	);
+});
+
+app.get('/database', function (req, res) {
+	req.db.exec('select SYSTEM_ID, DATABASE_NAME, HOST, VERSION, USAGE from M_DATABASE', function (err, results) {
+		if (err) {
+			res.type("text/plain").status(500).send("ERROR: " + err.toString());
+			return;
+		}
+		res.status(200).json(results);
+	});
+});
+
+app.get('/session', function (req, res) {
+	req.db.exec('select SESSION_USER, CURRENT_SCHEMA from DUMMY', function (err, results) {
+		if (err) {
+			res.type("text/plain").status(500).send("ERROR: " + err.toString());
+			return;
+		}
+		res.status(200).json(results);
+	});
+});
+
+app.get('/insert', function (req, res) {
+	var sql = 'insert into "test-mta-hdb::tables.test_table_1" ("createdAt") values(?)';
+	var ts = new Date().toISOString();
+	req.db.prepare(sql, function (err, statement) {
+		if (err) {
+			res.type("text/plain").status(500).send("ERROR: " + err.toString());
+			return;
+		}
+		statement.exec([ts], function (err, results) {
+			if (err) {
+				res.type("text/plain").status(500).send("ERROR: " + err.toString());
+			} else {
+				res.status(200).json(results);
+			}
+		});
+	});
+});
+
+app.get('/select', function (req, res) {
+	req.db.exec('select * from "test-mta-hdb::tables.test_table_1"', function (err, results) {
+		if (err) {
+			res.type("text/plain").status(500).send("ERROR: " + err.toString());
+			return;
+		}
+		res.status(200).json(results);
+	});
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+	console.info("Listening on port: " + port);
+});
